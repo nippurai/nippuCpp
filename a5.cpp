57cbeb9b0ba42ada2631eab8c919417f5f98dd28
Nippu Kumar <147337329+nippurai@users.noreply.github.com>
2023-10-08 22:55:52 +0530

    Create a5.cpp

---
+++ a5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float r,v;
+    cout<<"enter the value of r";
+    cin>>r;
+    v=3.14*r*r;
+    cout<<"volume "<<v;
+    return 0;
+    
+}
