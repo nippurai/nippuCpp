581c7269cfa3a2047ac8ec6e4a0bdc7f473dbe94
Nippu Kumar <147337329+nippurai@users.noreply.github.com>
2023-10-08 22:48:52 +0530

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,h;
+    cout<<"enter the value of b and h\n";
+    cin>>b>>h;
+    a=(b*h)/2;
+    cout<<"area "<<a;
+    return 0;
+}
