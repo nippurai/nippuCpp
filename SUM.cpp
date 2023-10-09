f95559a9bed7dc2048717f9ba991d26b06d878e4
Nippu Kumar <147337329+nippurai@users.noreply.github.com>
2023-10-09 23:19:18 +0530

    Create SUM.cpp

---
+++ SUM.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,sum;
+    cout<<"Enter the value of n\n";
+    cin>>n;
+    // formula of first n natural number is=n*(n+1)/2
+    sum=n*(n+1)/2;
+    cout<<"s is"<<sum<<endl;
+    return 0;
+}
