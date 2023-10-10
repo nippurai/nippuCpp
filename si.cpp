e707632296b5b3932722958ae0dbe2e7eb25b773
Nippu Kumar <147337329+nippurai@users.noreply.github.com>
2023-10-10 23:03:11 +0530

    Create si.cpp

---
+++ si.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float p,r,t,si;
+    cout<<"Enter the value of p r and t\n";
+    cin>>p>>r>>t;
+    // formula of calculate si is
+    si=p*r*t/100;
+    cout<<"simple is"<<si;
+    return 0;
+}
