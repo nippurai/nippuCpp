5bea72ca63e7462a9564e0a794fec574666e5a43
Nippu Kumar <147337329+nippurai@users.noreply.github.com>
2023-10-13 11:47:06 +0530

    Create area.cpp

---
+++ area.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float r,area;
+    cout<<"Enter the value of r\n";
+    cin>>r;
+    // formula of calculate area is
+    area=3.14*r*r;
+    cout<<"a"<<" "<<area<<endl;
+    return 0;
+}
