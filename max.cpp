2d3ee052a69f38853b94c2e17f46b1b5783a40fb
Nippu Kumar <147337329+nippurai@users.noreply.github.com>
2023-10-14 11:34:03 +0530

    Create max.cpp

---
+++ max.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y,max;
+    cout<<"enter the value of x and y\n";
+    cin>>x>>y;
+    if(x>y)
+    cout<<"x"<<endl;
+    else
+    cout<<"y"<<endl;
+    return 0;
+}
