e02ece5f8d53be45333c71ba01e53be7cec17fab
Nippu Kumar <147337329+nippurai@users.noreply.github.com>
2023-10-14 11:34:54 +0530

    Create p-n.cpp

---
+++ p-n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the value of n\n";
+    cin>>n;
+    if(n>0)
+    cout<<"positive"<<endl;
+    else
+    cout<<"negative"<<endl;
+    return 0;
+}
