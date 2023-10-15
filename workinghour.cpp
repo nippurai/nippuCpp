6368745d00f702e2f111341e0f5659c4a8f30219
Nippu Kumar <147337329+nippurai@users.noreply.github.com>
2023-10-15 13:03:17 +0530

    Create workinghour.cpp

---
+++ workinghour.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int hour;
+    cout<<"enter the value of hour\n";
+    cin>>hour;
+    if(hour>=9 &&hour<=18)
+    cout<<"working"<<endl;
+    else
+    cout<<"leisure"<<endl;
+}
