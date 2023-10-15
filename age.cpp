294692890874c946bbfe04801bff82643f447ff8
Nippu Kumar <147337329+nippurai@users.noreply.github.com>
2023-10-15 13:04:08 +0530

    Create age.cpp

---
+++ age.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int age;
+    cout<<"enter the value of age\n";
+    cin>>age;
+    if(age>=12 && age<=50)
+    cout<<"young"<<endl;
+    else
+    cout<<"not young"<<endl;
+}
