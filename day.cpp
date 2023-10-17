1da596402f3036ddeb887a3e9368957809ecc78f
Nippu Kumar <147337329+nippurai@users.noreply.github.com>
2023-10-17 20:49:07 +0530

    Create day.cpp

---
+++ day.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int day;
+    cout<<"enter the day";
+    cin>>day;
+    if(day==0)
+    cout<<"sunday"<<endl;
+   else if(day==1)
+    cout<<"monday"<<endl;
+   else if(day==2)
+    cout<<"tuesday"<<endl;
+    else if(day==3)
+    cout<<"wednesday"<<endl;
+    else if(day==4)
+    cout<<"thursday"<<endl;
+    else if(day==5)
+    cout<<"friday"<<endl;
+    else if(day==6)
+    cout<<"saturday"<<endl;
+    else
+    cout<<"sunday"<<endl;
+}
