816b8b36507ef30d790580af91cf654a273a3f45
Nippu Kumar <147337329+nippurai@users.noreply.github.com>
2023-10-18 20:08:26 +0530

    Create month.cpp

---
+++ month.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int month;
+    cout<<"enter the month\n";
+    cin>>month;
+    if(month==1)
+    cout<<"januray"<<endl;
+    else if(month==2)
+    cout<<"february"<<endl;
+    else if(month==3)
+    cout<<"march"<<endl;
+    else if(month==4)
+    cout<<"april"<<endl;
+    else if(month==5)
+    cout<<"may"<<endl;
+    else if(month==6)
+    cout<<"june"<<endl;
+    else if(month==7)
+    cout<<"july"<<endl;
+    else if(month==8)
+    cout<<"august"<<endl;
+    else if(month==9)
+    cout<<"september"<<endl;
+    else if(month==10)
+    cout<<"october"<<endl;
+    else if(month==11)
+    cout<<"november"<<endl;
+    else
+    cout<<"decemeber"<<endl;
+}
