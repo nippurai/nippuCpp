8a3fce3bdfd88865fbb22dd954bd12caf283607a
Nippu Kumar <147337329+nippurai@users.noreply.github.com>
2023-10-18 20:09:59 +0530

    Create digit.cpp

---
+++ digit.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int digit;
+    cin>>digit;
+    if(digit==1)
+    cout<<"one"<<endl;
+    else if(digit==2)
+    cout<<"two"<<endl;
+    else if(digit==3)
+    cout<<"three"<<endl;
+    else if(digit==4)
+    cout<<"four"<<endl;
+    else if(digit==5)
+    cout<<"five"<<endl;
+    else if(digit==6)
+    cout<<"six"<<endl;
+    else
+    cout<<"seven"<<endl;
+}
