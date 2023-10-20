dad5dccf40d65e9260935e4b9b2a857051563d75
Nippu Kumar <147337329+nippurai@users.noreply.github.com>
2023-10-20 12:12:25 +0530

    Create sum-L.cpp

---
+++ sum-L.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,sum;
+    cout<<"enter the value of n\n";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+    cout<<sum;
+
+return 0;
+}
